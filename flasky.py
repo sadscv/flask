ec2cc5fdb2800c776b7c2c33c70dd24c5735fce5
sadscv <sadscv@hotmail.com>
2016-10-15 12:38:59 +0800

    connect to mysql by pymysql

--- flasky.py
+++ flasky.py
@@ -1,18 +1,28 @@
 from flask import Flask
 from datetime import datetime as dt
 
+from flask import flash
 from flask import redirect
 from flask import render_template
 from flask import session
 from flask import url_for
-from flask.ext.bootstrap import Bootstrap
-from flask.ext.wtf import Form
+from flask_script import Shell
+from flask_script import Manager
+from flask_bootstrap import Bootstrap
+from flask_sqlalchemy import SQLAlchemy
+from flask_wtf import Form
 from wtforms import StringField, SubmitField, PasswordField
 from wtforms.validators import DataRequired, Email
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'hard to guess'
 bootstrap = Bootstrap(app)
+db = SQLAlchemy(app)
+manager = Manager(app)
+
+app.config['SQLALCHEMY_DATABASE_URI'] =\
+    'mysql+pymysql://flask:sadsad@localhost:3306/flask_blog'
+app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN'] = True
 
 class NameForm(Form):
     name = StringField('what is your name', validators = [DataRequired()])
@@ -23,9 +33,35 @@ class LoginForm(Form):
     password = PasswordField('password', validators=[DataRequired()])
     submit = SubmitField('Submit')
 
+
+class Role(db.Model):
+    __tablename__ = 'roles'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(64), unique=True)
+    users = db.relationship('User', backref='role')
+
+    def __repr__(self):
+        return '<Role %r>' % self.name
+
+class User(db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(64), unique=True, index=True)
+    role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
+
+    def __repr__(self):
+        return '<User %s>' % self.username
+
+
+def make_shell_context():
+    return dict(app=app, db=db, User=User, Role=Role)
+manager.add_command("shell", Shell(make_context=make_shell_context))
+
 @app.route('/')
 def hello_world():
     time = dt.now()
+    if session['name'] == 'sadscv@hotmail.com':
+        flash('OMG')
     return render_template('base.html', time=time)
 
 @app.route('/login/', methods=['GET', 'POST'])
@@ -33,7 +69,7 @@ def login():
     form = LoginForm()
     if form.validate_on_submit():
         session['name'] = form.username.data
-        return redirect('/test')
+        return redirect(url_for('hello_world'))
     return  render_template('login.html', form=form)
 
 
@@ -47,4 +83,4 @@ def login():
 
 
 if __name__ == '__main__':
-    app.run()
+    manager.run()
---
+++ test.py
@@ -0,0 +1,15 @@
+from sqlalchemy import Column
+from sqlalchemy import String
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+class User(Base):
+    __tablename__ = 'user'
+
+    id = Column(String(20), primary_key=True)
+    name = Column(String(20))
+
+engine = create_engine('mysql')
+
