2905ebd218ca9fc40e456a2ffb5848b92bbcb505
sadscv <sadscv@hotmail.com>
2016-10-17 11:21:56 +0800

    add password hash function and reletive testcase, add password_hash attribute on 'User' class

--- tests/test.py
+++ tests/test.py
@@ -2,6 +2,8 @@ import unittest
 
 from flask import current_app
 from app import create_app, db
+from app.models import User
+
 
 class BasicsTestCase(unittest.TestCase):
     def setUp(self):
@@ -20,3 +22,23 @@ class BasicsTestCase(unittest.TestCase):
 
     def test_app_is_testing(self):
         self.assertTrue(current_app.config['TESTING'])
+
+class UserModeTestCase(unittest.TestCase):
+    def test_password_setter(self):
+        u = User(password = 'test')
+        self.assertTrue(u.password_hash is not None)
+
+    def test_no_password_getter(self):
+        u = User(password = 'test')
+        with self.assertRaises(AttributeError):
+            u.password
+
+    def test_password_verification(self):
+        u = User(password = 'test')
+        self.assertTrue(u.verify_password('test'))
+        self.assertFalse(u.verify_password('false'))
+
+    def test_password_salts_are_random(self):
+        u1 = User(password = 'test')
+        u2 = User(password = 'test')
+        self.assertFalse(u1.password_hash == u2.password_hash)
