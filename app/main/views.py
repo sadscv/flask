a47365fa81c3c9b09433444f187fd2c8e53eb686
sadscv <sadscv@hotmail.com>
2016-10-20 22:14:37 +0800

    add edit_post,add css for post

--- app/main/views.py
+++ app/main/views.py
@@ -104,3 +104,17 @@ def edit_profile_admin(id):
     return render_template('edit_profile.html', form=form, user=user)
 
 
+@main.route('/edit/<int:id>', methods=['GET', 'POST'])
+@login_required
+def edit(id):
+    post = Post.query.get_or_404(id)
+    if current_user != post.author and not current_user.can(Permission.ADMIN):
+        abort(403)
+    form = PostForm()
+    if form.validate_on_submit():
+        post.body = form.body.data
+        db.session.add(post)
+        flash('post has been updated')
+        return redirect(url_for('.post', id=post.id))
+    form.body.data = post.body
+    return render_template('edit_post.html', form=form)
--- app/models.py
+++ app/models.py
@@ -164,10 +164,11 @@ class Post(db.Model):
     def on_changed_body(target, value, oldvalue, initiator):
         allowed_tags = ['a', 'abbr', 'acronym', 'b', 'blockquote', 'code',
                         'em', 'i', 'li', 'ol', 'pre', 'strong', 'ul',
-                        'h1', 'h2', 'h3', 'p', 'img']
-        target.body_html = bleach.linkify(bleach.clean(
-            markdown(value, output_format='html'),
-            tags=allowed_tags, strip=True))
+                        'h1', 'h2', 'h3', 'p', 'img',]
+        target.body_html = bleach.linkify(markdown(value, output_format='html'))
+        # target.body_html = bleach.clean(
+        #     markdown(value, output_format='html'),
+        #     tags=allowed_tags, strip=True)
 
     def __repr__(self):
         return '<Post %s>' % self.id
